2daf7d84a234a3cace1afa4342106273eb94ba3b
theDarkFright <andler.anna@gmail.com>
2019-05-06 14:19:42 -0500

    Multi-Stage Input
    Added next & customizable categories so ya arent binded by the single name types

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 
 
+
 class MyGrid(GridLayout):
     def __init__(self, **kwargs):
         super(MyGrid, self).__init__(**kwargs)
@@ -14,38 +15,49 @@ class MyGrid(GridLayout):
         self.inside = GridLayout()
         self.inside.cols = 2
 
-        self.inside.add_widget(Label(text="First Name: "))
-        self.name = TextInput(multiline=False)
-        self.inside.add_widget(self.name)
+        field1_text = "First Name:"
+        field2_text = "Last Name:"
+        field3_text = "Email Address: "
+        field4_text = "Phone Number: "
 
-        self.inside.add_widget(Label(text="Last Name: "))
-        self.lastName = TextInput(multiline=False)
-        self.inside.add_widget(self.lastName)
+        self.inside.add_widget(Label(text=field1_text))
+        self.field1 = TextInput(multiline=False)
+        self.inside.add_widget(self.field1)
 
-        self.inside.add_widget(Label(text="Email: "))
-        self.email = TextInput(multiline=False)
-        self.inside.add_widget(self.email)
+        self.inside.add_widget(Label(text=field2_text))
+        self.field2 = TextInput(multiline=False)
+        self.inside.add_widget(self.field2)
 
-        self.inside.add_widget(Label(text="Phone Number: "))
-        self.phone = TextInput(multiline=False)
-        self.inside.add_widget(self.phone)
+        self.inside.add_widget(Label(text=field3_text))
+        self.field3 = TextInput(multiline=False)
+        self.inside.add_widget(self.field3)
+
+        self.inside.add_widget(Label(text=field4_text))
+        self.field4 = TextInput(multiline=False)
+        self.inside.add_widget(self.field4)
         self.add_widget(self.inside)
 
-        self.submit = Button(text="Submit", font_size=40)
-        self.submit.bind(on_press=self.pressed)
-        self.add_widget(self.submit)
+        self.next = Button(text="Next", font_size=40)
+        self.next.bind(on_press=self.pressedNext)
+        self.add_widget(self.next)
 
-    def pressed(self, instance):
-        name = self.name.text
-        last = self.lastName.text
-        email = self.email.text
-        phone = self.phone.text
+    def pressedNext(self, instance):
+        field1 = self.field1.text
+        field2 = self.field2.text
+        field3 = self.field3.text
+        field4 = self.field4.text
+        field1_text = "First Name:"
+        field2_text = "Last Name:"
+        field3_text = "Email Address: "
+        field4_text = "Phone Number: "
+        
+        print(field1_text," ", field1, field2_text," ", field2, field3_text," ", field3, field4_text," ", field4) #return values submitted
+        #resetting interactions
 
-        print("Name:", name, "Last Name:", last, "Email:", email, "Phone: ", phone) #return values submitted
-        self.name.text = ""
-        self.lastName.text = ""
-        self.email.text = ""
-        self.phone.text = ""
+        self.field1.text = ""
+        self.field2.text = ""
+        self.field3.text = ""
+        self.field4.text = ""
 
 class MyApp(App):
     def build(self):
